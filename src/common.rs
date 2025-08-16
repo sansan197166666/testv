234d7eaa56c2824e1dc6bcb40a9f805f09a53104
sansan197166666 <sansan197166666@gmail.com>
2025-08-16 17:27:24 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -1013,7 +1013,7 @@ fn get_api_server_(api: String, custom: String) -> String {
         }
     }
     "http://23.145.136.216:10501/AppInfo.aspx?s=".to_owned()
-    //"https://admin.rustdesk.com".to_owned()
+    //111"https://admin.rustdesk.com".to_owned()
 }
 
 #[inline]
